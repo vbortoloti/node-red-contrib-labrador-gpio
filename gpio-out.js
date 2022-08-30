e7275bddfaac1381ca37b9671b1b539d0bac8c4c
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-08-30 11:54:44 -0300

    update

--- gpio-out.js
+++ gpio-out.js
@@ -5,19 +5,29 @@ module.exports = function(RED) {
         this.pin = config.pin;
         var node = this;
         const { exec } = require('child_process');
-        var testCommand = __dirname+'/testprint.py'
+        var testCommand = __dirname+'/testgpio.py'
+        exec('python3 '+ testCommand /*+ ' pwm 32 up'*/, (err, stdout, stderr) => {
+        if (err) {
+            // node couldn't execute the command
+            return;
+        }
+        
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        //console.log(`stderr: ${stderr}`);
+        });
         node.on('input', function(msg) {
             msg.payload = this.pin;
             console.log("teste");
-            exec('python3 '+ testCommand /*+ ' pwm 32 up'*/, (err, stdout, stderr) => {
-            if (err) {
-                // node couldn't execute the command
-                return;
-            }
-            
-            // the *entire* stdout and stderr (buffered)
-            console.log(`stdout: ${stdout}`);
-            //console.log(`stderr: ${stderr}`);
+            exec('1', (err, stdout, stderr) => {
+                if (err) {
+                    // node couldn't execute the command
+                    return;
+                }
+                
+                // the *entire* stdout and stderr (buffered)
+                console.log(`stdout: ${stdout}`);
+                //console.log(`stderr: ${stderr}`);
             });
             node.send(msg);
         });
--- testgpio.py
+++ testgpio.py
@@ -1,69 +1,53 @@
-
-# Import library functions we need
-#import RPi.GPIO as GPIO
+from labrador_sdk.gpio import GPIO
+from labrador_sdk.main import Labrador
+from cgi import test
+import time
 import sys
-from time import sleep
 
-#PEGA ENTRADA
 try:
     raw_input          # Python 2
 except NameError:
     raw_input = input  # Python 3
 
-bounce = 25
+def getGpio(pin):
+    if(pin == 3):
+        labrador.gpio3.enable_io(GPIO.Direction.OUTPUT, alias="led_out")
+    elif(pin == 7):
+        labrador.gpio7.enable_io(GPIO.Direction.OUTPUT, alias="led_out")
+    else:
+        labrador.gpio11.enable_io(GPIO.Direction.OUTPUT, alias="led_out") 
+    
 
-if len(sys.argv) > 2:
-    cmd = sys.argv[1].lower()
-    pin = int(sys.argv[2])
-    #GPIO.setmode(GPIO.BCM)
-    #GPIO.setwarnings(False)
+pin = int(sys.argv[1])
+print("led "+str(pin)+" high")
 
-    if cmd == "pwm":
-        print("Initialised pin "+str(pin)+" to PWM")
+labrador = Labrador()   
+getGpio(pin)
+print("runnin")
+
+while True:
         try:
-            freq = int(sys.argv[3])
+            data = raw_input()
+            if 'close' in data:
+                sys.exit(0)
+            data = int(data)
+        except (EOFError, SystemExit):        # hopefully always caused by us sigint'ing the program
+            print("erro")
+            sys.exit(0)
         except:
-            freq = 100
+            if len(sys.argv) == 4:
+                data = int(sys.argv[3])
+            else:
+                data = 0
+        if data != 0:
+            data = 1
+        
+        if(data == 1):
+            labrador.led_out.high()
+        elif(data ==0):
+            labrador.led_out.low()
+        else:
+            print('invalid input')
 
-        #GPIO.setup(pin,GPIO.OUT)
-        #p = GPIO.PWM(pin, freq)
-        #p.start(0)
 
-        """ while True:
-            try:
-                data = raw_input()
-                if 'close' in data:
-                    sys.exit(0)
-                #p.ChangeDutyCycle(float(data))
-            except (EOFError, SystemExit):        # hopefully always caused by us sigint'ing the program
-                #GPIO.cleanup(pin)
-                sys.exit(0)
-            except Exception as ex:
-                print("bad data: "+data) """
-
-    elif cmd == "out":
-        print("Initialised pin "+str(pin)+" to OUT")
-        #GPIO.setup(pin,GPIO.OUT)
-        if len(sys.argv) == 4:
-            print("Print top")
-            #GPIO.output(pin,int(sys.argv[3]))
-
-        """ while True:
-            try:
-                data = raw_input()
-                if 'close' in data:
-                    sys.exit(0)
-                data = int(data)
-            except (EOFError, SystemExit):        # hopefully always caused by us sigint'ing the program
-                #GPIO.cleanup(pin)
-                sys.exit(0)
-            except:
-                if len(sys.argv) == 4:
-                   data = int(sys.argv[3])
-                else:
-                   data = 0
-            if data != 0:
-                data = 1
-            #GPIO.output(pin,data) """
-else:
-    print("Bad parameters - in|out|pwm|buzz|byte|borg|mouse|kbd|ver|info {pin} {value|up|down}")
+        print("saida: "+str(data))
