67e10a0980cebdaab32a1b1f9d78ac35f9d12f95
Vitor Bortoloti <55714338+vbortoloti@users.noreply.github.com>
2022-10-15 15:53:20 -0300

    att

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import caninos_sdk as k9
 labrador = k9.Labrador()
 
 # habilita o pino 15 como saída, e dá a ele o apelido "led_status"
-labrador.pin15.enable_gpio(k9.Pin.Direction.OUTPUT, alias="led_status")
+labrador.pin3.enable_gpio(k9.Pin.Direction.OUTPUT, alias="led_status")
 
 # liga o "led_status"
 labrador.led_status.high()
