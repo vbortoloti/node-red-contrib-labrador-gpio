77066c6e208298d13ae343690e24326f9255fd24
Vitor Bortoloti <55714338+vbortoloti@users.noreply.github.com>
2022-10-16 19:24:35 -0300

    s

--- gpio-out.js
+++ gpio-out.js
@@ -17,26 +17,11 @@ module.exports = function(RED) {
         node.child = spawn(gpioCommand, [this.pin]);
         console.log("Spawning child process");
 
-        function check_input(msg){
-            if (msg.payload === "true" || msg.payload === 1) { msg.payload = true; out = 1;}
-            if (msg.payload === "false" || msg.payload === 0) { msg.payload = false; out = 0;}
-            return msg;
-        }
-
         function inputlistener(msg, send, done) {
             var out;
-            if(msg.payload === "true"){ msg.payload = true;out =1}
-            if(msg.payload === "false"){ msg.payload = false;out =0}
-
-            msg.payload = check_input(msg.payload);
-            out = check_input(msg.payload);
-            console.log(this.pin);
-            console.log(this.iotype);
-            console.log(this.freq);
-            console.log(this.initstate);
-            console.log(this.set);
-
-            /* -- Infinite Loop -- */
+            if (msg.payload === "true") { msg.payload = true; out = 1 }
+            if (msg.payload === "false") { msg.payload = false; out = 0}
+            
             if (node.child !== null) {
                 node.child.stdin.write(out+"\n", () => {
                     if (done) { done(); }
@@ -44,10 +29,8 @@ module.exports = function(RED) {
             }else {
                 console.log("erro")
             }
-            
             node.send(msg);
         }
-
         node.on('input',inputlistener);
  
     }
