5e94bb52e9ffbb5a0b664db19e4afb4f158fd915
Vitor Bortoloti <55714338+vbortoloti@users.noreply.github.com>
2022-10-30 16:36:06 -0300

    attg

---
+++ gpio-in.js
@@ -0,0 +1,41 @@
+//npm install python-shell
+module.exports = function(RED) {
+    function GpioIn(config) {
+        RED.nodes.createNode(this,config);
+
+        this.pin = config.pin;
+        this.read = config.read || false;
+        
+
+        var node = this;
+        var gpioCommand = __dirname+'/gpio-in.sh';
+        var spawn = require("child_process").spawn;
+        //FIX ME
+        //node.child = spawn(gpioCommand, [this.pin, out, 100, 0, false]);
+        node.child = spawn(gpioCommand,[this.pin]);
+
+        console.log("Spawning child process");
+        var out = 0;
+       
+        node.child.stdout.on('data', function (data) {
+            var d = data.toString().trim().split("\n");
+            for (var i = 0; i < d.length; i++) {
+                if (d[i] === '') { return; }
+                else{node.send({ topic:"gpio/"+node.pin, payload:Number(d[i]) });}
+            }
+        });
+
+        node.on("close", function(done) {
+            if (node.child != null) {
+                node.finished = done;
+                node.child.stdin.write("close "+node.pin, () => {
+                    node.child.kill('SIGKILL');
+                    setTimeout(function() { if (done) { done(); } }, 50);
+                });
+            }
+            else { if (done) { done(); } }
+        });
+ 
+    }
+    RED.nodes.registerType("gpio-in",GpioIn);
+}
