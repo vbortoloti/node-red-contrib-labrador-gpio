5c4d35988b9191ea05808791a7e6a377f5a382cf
Vitor Bortoloti <55714338+vbortoloti@users.noreply.github.com>
2022-10-30 18:28:31 -0300

    te

--- gpio-in.py
+++ gpio-in.py
@@ -15,20 +15,28 @@ def getGpio(labrador, pin_to_enable):
         pin = getattr(labrador, pin_to_enable)
         pin.enable_gpio(k9.Pin.Direction.INPUT, alias="input")
 
+def Read():
+    while run:
+        read  = labrador.input.read()
+        if read != lastRead:
+            lastRead = read
+            print(read)
+
 pin = int(sys.argv[1])
-
-
 labrador = k9.Labrador()
 getGpio(labrador,pin)
-
 lastRead  = labrador.input.read()
 print(lastRead)
 
+run = True
+readThread = threading.Thread(target=Read)
+readThread.start()
+
 while True:
-    read  = labrador.input.read()
-    if read != lastRead:
-        lastRead = read
-        print(read)
+    data = raw_input()
+        run = False
+        sys.exit(0)
+
 
 
 
