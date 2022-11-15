f0c30ea6b16e5261960fb9ba9df359bad417a79c
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-11-15 18:35:26 -0300

    w

--- gpio-out.py
+++ gpio-out.py
@@ -15,9 +15,17 @@ def getGpio(labrador, pin_to_enable):
         pin = getattr(labrador, pin_to_enable)
         pin.enable_gpio(k9.Pin.Direction.OUTPUT, alias="led_out")
 
+def getGpioPwm(labrador, pin_to_enable):
+    pin_to_enable = f"pin{pin_to_enable}"
+    if hasattr(labrador, pin_to_enable):
+        pin = getattr(labrador, pin_to_enable)
+        pin.enable_pwm(alias="pwm_out", freq=frequency, duty_cycle=duty)
+
 pin = int(sys.argv[1])
 mode = str(sys.argv[2])
-freq = int(sys.argv[3])
+frequency = int(sys.argv[3])
+duty = int(sys.argv[4])
+
 # init_state = int(sys.argv[4])
 # begin_with_init_state = str(sys.argv[5])
 
@@ -25,7 +33,7 @@ print("led "+str(pin)+" high")
 print("led "+str(pin)+" high")
 
 labrador = k9.Labrador()   
-getGpio(labrador,pin)
+
 # if(begin_with_init_state == "true"):
 #     if(init_state == 1):
 #         print("Led High")
@@ -33,24 +41,50 @@ getGpio(labrador,pin)
 
 print("running")
 
-while True:
-        try:
-            data = raw_input()
-            if 'close' in data:
+if mode == "out":
+    getGpio(labrador,pin)
+    while True:
+            try:
+                data = raw_input()
+                if 'close' in data:
+                    sys.exit(0)
+                data = int(data)
+            except (EOFError, SystemExit):        # hopefully always caused by us sigint'ing the program
+                print("erro")
                 sys.exit(0)
-            data = int(data)
-        except (EOFError, SystemExit):        # hopefully always caused by us sigint'ing the program
-            print("erro")
-            sys.exit(0)
 
-        if(data == 1):
-            print("Led High")
-            labrador.led_out.high()
-        elif(data ==0):
-            print("Led Low")
-            labrador.led_out.low()
-        else:
-            print('invalid input')
-            break
+            if(data == 1):
+                print("Led High")
+                labrador.led_out.high()
+            elif(data ==0):
+                print("Led Low")
+                labrador.led_out.low()
+            else:
+                print('invalid input')
+                break
 
-        print("saida: "+str(data))
+            print("saida: "+str(data))
+
+elif mode == "pwm":
+    getGpioPwm(labrador,pin)
+    while True:
+            try:
+                data = raw_input()
+                if 'close' in data:
+                    sys.exit(0)
+                data = int(data)
+            except (EOFError, SystemExit):        # hopefully always caused by us sigint'ing the program
+                print("erro")
+                sys.exit(0)
+
+            if(data == 1):
+                print("Led High")
+                labrador.pwm_out.start()
+            elif(data ==0):
+                print("Led Low")
+                labrador.pwm_out.stop()
+            else:
+                print('invalid input')
+                break
+
+            print("saida: "+str(data))
